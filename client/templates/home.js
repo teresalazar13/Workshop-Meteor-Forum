35298e8c982b514c8ce897cc00de593de5a7a282
teresalazar13 <teresa.sal13@gmail.com>
2017-02-07 13:47:56 +0000

    Adds list of questions.

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,5 +1,14 @@
 Template.home.helpers({
 
+  listOfQuestions: function() {
+    list = [
+      {text: 'Tu queres tu tens?'},
+      {text: 'Soma e segue?'},
+      {text: 'Tudo bem?'},
+    ];
+    return list;
+  },
+
 });
 
 Template.home.events({
